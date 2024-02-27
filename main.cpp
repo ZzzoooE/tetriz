e3f7deb311c0c46de9fed322b4781ceed2af8a3d
z <jly>
2024-02-27 23:05:23 +0800

    终端控制

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include "terminal.h"
+#include <chrono>
+#include <thread>
 
-using namespace std;
+using namespace std::chrono_literals;
 
 int main() {
-    cout << "hellow world\n";
-    return 0;
+   tc::hide_cursor();
+
+   int i = 1;
+   while (true) {
+      tc::clean_screen();
+      tc::move_to(i ++ , 10);
+      tc::set_back_color(15);
+      std::cout << "  ";
+      std::cout << std::flush;
+      tc::reset_color();
+      std::this_thread::sleep_for(500ms);
+   }
+   return 0;
 }
